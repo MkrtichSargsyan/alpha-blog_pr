fdf58cb3fd98f6aadf3ff9f26b2a2a1113d47f3c
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-10-20 09:36:41 +0400

    create Article model

--- db/migrate/20201018193936_creaate_articles.rb
+++
@@ -1,4 +0,0 @@
-class CreaateArticles < ActiveRecord::Migration[6.0]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_193936) do
+ActiveRecord::Schema.define(version: 2020_10_19_035854) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
