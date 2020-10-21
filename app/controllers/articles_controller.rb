b72463888239ac9d4c78edf479d0d706aac6b12b
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-10-21 08:49:47 +0400

    add create and new actions on articles controller for creating new article from browser

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,13 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new; end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render json:@article
+    @article.save
+    redirect_to article_path(@article)
+  end
 end
