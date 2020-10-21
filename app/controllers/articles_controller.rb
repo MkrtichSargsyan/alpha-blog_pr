0e598c521e46ff4141dfa7b183d4c74dd3167aae
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-10-21 09:46:02 +0400

    add flash notice to show if article was successfully created and edit new.html.rb file to show errors if article was not created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,12 +7,19 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def new; end
+  def new
+    @article = Article.new
+  end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     # render json:@article
-    @article.save
-    redirect_to article_path(@article)
+
+    if @article.save
+      flash[:notice] = 'Article was created successfully'
+      redirect_to article_path(@article) # @article is the same
+    else
+      render 'new'
+    end
   end
 end
