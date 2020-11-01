c0ec9967bcb80ad097980f07a861f5c8d85f815f
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-01 21:00:09 +0400

    add view for signup user,create new action for creating new user,make errors file global for both users and articles,add routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
 
   resources :articles
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
