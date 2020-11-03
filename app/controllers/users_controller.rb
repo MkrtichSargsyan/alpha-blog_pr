bb0c23c169295b1cf4dd3e8fad49a3b269d6cb1b
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-03 14:30:01 +0400

    add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,18 +18,23 @@ class UsersController < ApplicationController
   end
 
   def update
-     @user = User.find(params[:id])
-     if @user.update(user_params)
-        flash[:notice] = 'Your account information was successfully updated'
-        redirect_to articles_path
-     else
-        render 'edit'
-     end
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'Your account information was successfully updated'
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username,:email,:password)
+    params.require(:user).permit(:username, :email, :password)
   end
 end
