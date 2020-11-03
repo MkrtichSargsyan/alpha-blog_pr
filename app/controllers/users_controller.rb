9bd92951eee5a45cd6a037007331c56cd1b2797d
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-03 15:29:01 +0400

    add index action to user controller,create gravatar_for method in helper file for getting image associated with email,add blogger link on navigation layout to show all users,create view for showing all registered users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def gravatar_for(user,options = {size:80})
+  def gravatar_for(user, options = { size: 80 })
     email_address = user.email.downcase
     hash = Digest::MD5.hexdigest(email_address)
     size = options[:size]
