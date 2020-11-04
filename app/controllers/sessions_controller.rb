c12e930963027466b06962a7eac1a6acbbc9b276
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-04 19:44:05 +0400

    add login template

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+      byebug
+  end
+  def destroy
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   # post 'users', to: 'users#create'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
