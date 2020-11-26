d00af57d6d99ee08821256396f33e2be14fa2b7c
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-11-26 05:53:02 +0400

    change navigation to show user dropdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-
-  helper_method :current_user,:logged_in?
+  helper_method :current_user, :logged_in?
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -9,4 +8,9 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
 
+  def require_user
+    if !logged_in?
+      flash[:alert] = 'you must be logged in to perform that action'
+    end
+  end
 end
